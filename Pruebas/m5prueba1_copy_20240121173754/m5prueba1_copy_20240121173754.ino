8e35697879198548c2c33b90ca28ab2b6aa3c18f
alfonsín <ponchodeandan@gmail.com>
2024-01-22 21:44:47 -0600

    M5Stack Prueba de botones

--- Pruebas/m5prueba1_copy_20240121173754/m5prueba1_copy_20240121173754.ino
+++ Pruebas/m5prueba1_copy_20240121173754/m5prueba1_copy_20240121173754.ino
@@ -1,37 +1,97 @@
 #include <M5Stack.h>
 
-int estado=1;
+int estadoA=0;
+int estadoB=0;
+int estadoC=0;
 
 void setup() {
-    M5.begin();        // Init M5Core. 
-    M5.Power.begin();  // Init Power module.  
-    M5.Lcd.setTextColor(GREEN);  // Set the font color to yellow. 
-    M5.Lcd.setTextSize(4);     // Set the font size. 
+    M5.begin();        
+    M5.Power.begin();   
+    M5.Lcd.setTextColor(GREEN);  
+    M5.Lcd.setTextSize(4);     
     Serial.begin(9600);
 }
 
 void loop() {
-  M5.update();    //You need to add M5.update () to read the status of the keystroke. For details, please s((ee System. 
+  M5.update(); 
+  BotonA();
+  BotonB();
+  BotonC();
+}
+
+
+void BotonA(){
   if(M5.BtnA.wasPressed()){
-    Serial.println(estado);
-    if(estado==1) {
-      estado=0;
+    Serial.println(estadoA);
+    if(estadoA==1) {
+      estadoA=0;
     }
-    else if(estado==0) {
-      estado=1;
+    else if(estadoA==0) {
+      estadoA=1;
     }
   }
-
   M5.Lcd.setCursor(70,90);
-  if (estado==0) {    //If the key is pressed. 
+  if (estadoA==0) { 
     M5.Lcd.clear(BLACK);  
     M5.Lcd.println("Jugando");
     delay(100);
   }
-  else if(estado==1){
+  else if(estadoA==1){
     M5.Lcd.clear(BLACK);
     M5.Lcd.setCursor(50,90);
     M5.Lcd.println("Jugandon't");
+    delay(100);
+  }
+  delay(20);
+}
+
+
+void BotonB(){
+  if(M5.BtnB.wasPressed()){
+    Serial.println(estadoB);
+    if(estadoB==1) {
+      estadoB=0;
+    }
+    else if(estadoB==0) {
+      estadoB=1;
+    }
+  }
+  M5.Lcd.setCursor(70,90);
+  if (estadoB==0) {   
+    M5.Lcd.clear(BLACK);  
+    M5.Lcd.println("Calibrando dirección");
+    delay(100);
+  }
+  else if(estadoB==1){
+    M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(50,90);
+    M5.Lcd.println(":)");
+    delay(100);
+  }
+  delay(20);
+}
+
+
+void BotonC(){
+  if(M5.BtnC.wasPressed()){
+    Serial.println(estadoC);
+    if(estadoC==1) {
+      estadoC=0;
+    }
+    else if(estadoC==0) {
+      estadoC=1;
+    }
+  }
+  M5.Lcd.setCursor(70,90);
+  if (estadoC==0) {  
+    M5.Lcd.clear(BLACK);  
+    M5.Lcd.println("Calibrando pista");
+    delay(100);
+  }
+  else if(estadoC==1){
+    M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(50,90);
+    M5.Lcd.println(":)");
     delay(100);
   }
   delay(20);
